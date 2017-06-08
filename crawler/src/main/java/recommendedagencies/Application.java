a478a17c7dfea69bd03beac6e8244e5c98d22381
Matej Ferenc <matej.ferenc@monster.com>
2017-06-08 09:01:02 +0200

    page downloader

--- crawler/src/main/java/recommendedagencies/Application.java
+++ crawler/src/main/java/recommendedagencies/Application.java
@@ -3,45 +3,52 @@ package recommendedagencies;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Application {
 
     private static final String BASE_URL = "http://search.recommendedagencies.com/api/v1/search?services[]=digital/web-development/&offset=";
 
+    private static final List<String> subPages = Arrays.asList("", "clients", "staff", "finances", "rate-card", "contact");
+
     public static void main(String[] args) throws IOException {
-        getPage();
+        downloadPages();
     }
 
-    private static void getPage() throws IOException {
-        ClassLoader classLoader = Application.class.getClassLoader();
-        File file = new File(classLoader.getResource("main_data.json").getFile());
-        ObjectMapper mapper = new ObjectMapper();
-        HashMap map = mapper.readValue(file, HashMap.class);
+    private static void downloadPages() throws IOException {
+        HashMap map = readMainData();
+        File companyDirectory = new File("companies");
+        companyDirectory.mkdir();
 
-        List<String> collect = ((List<HashMap>) map.get("response")).stream()
-                .map(company -> ((Map) company).get("title"))
-                .map(title -> (String) title)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet()
-                .stream()
-                .filter(p -> p.getValue() > 1)
-                .map(e -> e.getKey())
-                .collect(Collectors.toList());
-
-
-//        ((List) map.get("response"))
-//                .forEach(company -> {
-//                    String title = ((String)((Map) company).get("title"));
-//                    String pageUrl = HttpUtils.get((String)((Map) company).get("url"));
-////                    String page = HttpUtils.get(pageUrl);
-//                });
+        List<HashMap> responseList = (List<HashMap>) map.get("response");
+        for (int i = 0; i < responseList.size(); i++) {
+            HashMap company = responseList.get(i);
+            String id = ((String) company.get("id"));
+            String companyUrl = (String) company.get("url");
+            String companyTitle = (String) company.get("title");
+            System.out.println("Page " + companyTitle + " started");
+            File companyFolder = new File(companyDirectory, id);
+            companyFolder.mkdir();
+            subPages.forEach(subPage -> {
+                String pageUrl = companyUrl + subPage;
+                String pageSourceCode = HttpUtils.get(pageUrl);
+                File subPageSourceCodeFile = new File(companyFolder, subPage + ".html");
+                try {
+                    PrintWriter writer = new PrintWriter(subPageSourceCodeFile);
+                    writer.print(pageSourceCode);
+                } catch (FileNotFoundException e) {
+                    throw new IllegalStateException(e);
+                }
+                System.out.println("Page " + subPage + " downloaded");
+            });
+            System.out.println("Page " + companyTitle + " finished");
+        }
         boolean stop = true;
     }
 
@@ -50,6 +57,13 @@ public class Application {
         ObjectMapper mapper = new ObjectMapper();
         Object o = mapper.readValue(s, Object.class);
         mapper.writeValue(new File("main_data.json"), o);
+    }
+
+    private static HashMap readMainData() throws IOException {
+        ClassLoader classLoader = Application.class.getClassLoader();
+        File file = new File(classLoader.getResource("main_data.json").getFile());
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(file, HashMap.class);
     }
 
 }
